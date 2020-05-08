29e9a8c98c0fd591ff7fb3954f8f7033b723b1d8
nicozar95 <nzarewsky@gmail.com>
2020-05-08 14:50:46 -0300

    Add migration and schema

---
+++ migrations/migrations/20200508171011-create-users.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    }),
+
+  // eslint-disable-next-line no-unused-vars
+  down: (queryInterface, _Sequelize) => queryInterface.dropTable('users')
+};
