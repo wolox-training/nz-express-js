8cac1d7aa27fc95eac10f0c97af796ce4cc59bb0
nicozar95 <nzarewsky@gmail.com>
2020-05-13 09:30:27 -0300

    Fix on PR;

--- app/services/number.js
+++ app/services/number.js
@@ -1,3 +1,10 @@
 const axios = require('axios');
+const logger = require('../logger');
 
-exports.getFacts = number => axios.get(`http://numbersapi.com/${number}`).then(({ data }) => data);
+exports.getFacts = number => {
+  const url = process.env.NUMBERS_BASE_URL + number;
+  axios.get(url).then(({ data }) => {
+    logger.info(`Fetch number ${number} fact at ${url}: ${data}`);
+    return data;
+  });
+};
