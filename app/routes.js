07d5510f8a191485bcfd150481197d9f9b545d64
nicozar95 <nzarewsky@gmail.com>
2020-05-28 14:28:55 -0300

    Creqte admin user

--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,11 @@ const paginate = require('express-paginate');
 const { healthCheck } = require('./controllers/healthCheck');
 const { createUser, indexUser } = require('./controllers/usersController');
 const { createSession } = require('./controllers/sessionsController');
+const { createAdminUser } = require('./controllers/adminController');
 
 const { validateSchema } = require('./middlewares/schema_validator');
 const { validatePassword, authenticateEndpoint } = require('./middlewares/session');
+const { validateAdminUser } = require('./middlewares/admin');
 
 const { userSchema } = require('./schemas/user');
 const { sessionSchema } = require('./schemas/session');
@@ -16,4 +18,10 @@ exports.init = app => {
   app.post('/users', [validateSchema(userSchema)], createUser);
   app.get('/users', [authenticateEndpoint, paginate.middleware(3, 5)], indexUser);
   app.post('/users/sessions', [validateSchema(sessionSchema), validatePassword], createSession);
+
+  app.post(
+    '/admin/users',
+    [authenticateEndpoint, validateAdminUser, validateSchema(userSchema)],
+    createAdminUser
+  );
 };
