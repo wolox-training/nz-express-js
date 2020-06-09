d37fb753d20cbc4c67bcff9618d2d288843bc5bc
nicozar95 <nzarewsky@gmail.com>
2020-06-09 14:15:05 -0300

    Fix on PR

--- app/middlewares/session.js
+++ app/middlewares/session.js
@@ -4,28 +4,37 @@ const util = require('util');
 const { findUserByEmail } = require('../services/user');
 const { sessionError } = require('../errors');
 
-const findUser = (request, _response, next) => {
-  // eslint-disable-next-line consistent-return
-  findUserByEmail(request.body.email).then(user => {
-    if (!user) return next(sessionError('User not found'));
-    request.user = user;
+// eslint-disable-next-line consistent-return
+const findUser = async (request, _response, next) => {
+  const userEmail = request.body.email;
+
+  try {
+    const dbUser = await findUserByEmail(userEmail);
+    if (!dbUser) return next(sessionError('User not found'));
+    // eslint-disable-next-line require-atomic-updates
+    request.user = dbUser;
     next();
-  });
+  } catch (err) {
+    next(err);
+  }
 };
 
-const comparePassword = (request, _response, next) => {
+const comparePassword = async (request, _response, next) => {
   const givenPassword = request.body.password;
   const { user } = request;
 
   const comparePasswordPromise = util.promisify(bcrypt.compare);
 
-  comparePasswordPromise(givenPassword, user.password).then(res => {
-    if (res) {
+  try {
+    const compareResult = await comparePasswordPromise(givenPassword, user.password);
+    if (compareResult) {
       next();
     } else {
       next(sessionError('Password not match'));
     }
-  });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.validatePassword = [findUser, comparePassword];
