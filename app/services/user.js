3fb9a7042bd122e894fc072d543cfa662a9b44d2
nicozar95 <nzarewsky@gmail.com>
2020-06-16 14:10:51 -0300

    Fix on PR

--- app/services/user.js
+++ app/services/user.js
@@ -32,11 +32,3 @@ exports.listUser = request => {
     throw databaseError('Unable to list users');
   });
 };
-
-exports.listUser = request => {
-  logger.info('Listing users...');
-  return User.findAndCountAll({
-    limit: request.query.limit,
-    offset: request.skip
-  });
-};
